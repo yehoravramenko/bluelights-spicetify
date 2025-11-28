f32785f1c4d8bb60fa5a90602b589d974498b1f9
Yehor Avramenko <yehor.avramenko@ukr.net>
2025-11-28 19:44:48 +0100

    blank project

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+function main() {
+  while (!Spicetify?.showNotification) {
+    await new Promise(resolve => setTimeout(resolve, 100));
+  }
+  
+  // Show message on start.
+  Spicetify.showNotification("Hello!");
+}
+
+export default main;
