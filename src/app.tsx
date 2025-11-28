4a3879952a8d3b7074fbac3a67fff240d868897e
Yehor Avramenko <yehor.avramenko@ukr.net>
2025-11-28 20:17:20 +0100

    Get color from spicetify

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,27 @@
-function main() {
-  while (!Spicetify?.showNotification) {
-    await new Promise(resolve => setTimeout(resolve, 100));
-  }
-  
-  // Show message on start.
-  Spicetify.showNotification("Hello!");
+let currentColor: Spicetify.rgb;
+
+async function handleSongChange(): Promise<void> {
+    //Spicetify.showNotification("song changed");
+
+    const currentTrack = Spicetify.Player.data.item;
+    const colors = await Spicetify.extractColorPreset(currentTrack.metadata.image_url);
+    const selectedColor = colors[0].colorRaw.rgb;
+    Spicetify.showNotification(`${selectedColor.r}, ${selectedColor.g}, ${selectedColor.b}`);
+
+    if(selectedColor === currentColor) {
+        return;
+    }
+
+    currentColor = selectedColor;
+}
+
+function main(): void {
+    Spicetify.Player.addEventListener("songchange", handleSongChange);
+    Spicetify.Player.addEventListener("onplaypause", (event) => {
+        if(!event?.data.isPaused) {
+            handleSongChange();
+        }
+    });
 }
 
 export default main;
