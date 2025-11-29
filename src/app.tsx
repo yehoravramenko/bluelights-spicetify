14b9ed7eb2b05a401e701b0cf5b2a29b6aedc994
Yehor Avramenko <yehor.avramenko@ukr.net>
2025-11-29 04:00:13 +0100

    Send color data to local TCP server

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 let currentColor: Spicetify.rgb;
+// let client = new net.Socket();
 
 async function handleSongChange(): Promise<void> {
     //Spicetify.showNotification("song changed");
@@ -6,13 +7,27 @@ async function handleSongChange(): Promise<void> {
     const currentTrack = Spicetify.Player.data.item;
     const colors = await Spicetify.extractColorPreset(currentTrack.metadata.image_url);
     const selectedColor = colors[0].colorRaw.rgb;
-    Spicetify.showNotification(`${selectedColor.r}, ${selectedColor.g}, ${selectedColor.b}`);
-
+//    Spicetify.showNotification(`${selectedColor.r}, ${selectedColor.g}, ${selectedColor.b}`);
+    
     if(selectedColor === currentColor) {
         return;
     }
 
     currentColor = selectedColor;
+
+    console.log(currentColor.r, currentColor.g, currentColor.b);
+    const colorData = `${currentColor.r.toString(16).padStart(2, '0')}${currentColor.g.toString(16).padStart(2, '0')}${currentColor.b.toString(16).padStart(2, '0')}`;
+
+    Spicetify.showNotification(colorData);
+
+    const url = "http://127.0.0.1:6969";
+    const response = await fetch(url, {method: 'POST', body: colorData})
+        .catch(error => console.log("error: ", error))
+    
+    if(!response.ok) {
+        console.log("damn");
+        return;
+    }
 }
 
 function main(): void {
